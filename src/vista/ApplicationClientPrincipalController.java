1c81381d25af2504957a5d004c844f7cf7e98a68
buunnee <lucianbunescu@gmail.com>
2024-10-20 22:59:24 +0200

    Controlador del Main

--- src/vista/ApplicationClientPrincipalController.java
+++ src/vista/ApplicationClientPrincipalController.java
@@ -1,27 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package vista;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import negocio.Client;
+import utilidades.User;
 
 /**
- * FXML Controller class
- *
- * @author 2dam
+ * Controlador para la pantalla principal de la aplicación.
  */
 public class ApplicationClientPrincipalController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    private static final Logger LOGGER = Logger.getLogger(ApplicationClientPrincipalController.class.getName());
+    private Stage stage;
+    private Client client;
+    private User usuarioActual;
+
+    @FXML
+    private Label welcomeLabel;
+    @FXML
+    private Button logoutButton;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
     }
 
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public void setUsuarioActual(User usuarioActual) {
+        this.usuarioActual = usuarioActual;
+        actualizarMensajeBienvenida();
+    }
+
+    private void actualizarMensajeBienvenida() {
+        if (usuarioActual != null) {
+            welcomeLabel.setText("¡Bienvenido! " + usuarioActual.getName());
+        } else {
+            LOGGER.severe("El usuario actual es nulo. No se puede mostrar el nombre.");
+        }
+    }
+
+    @FXML
+    private void cerrarSesion(ActionEvent event) {
+        LOGGER.info("El usuario ha presionado el botón de cerrar sesión.");
+        if (stage != null) {
+            stage.close(); 
+        } else {
+            LOGGER.severe("El stage es nulo. No se puede cerrar la ventana.");
+        }
+    }
 }
