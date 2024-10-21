68a4bb71dcdf96d465e16a832f9ad0c72bc56f97
2dam <2dam@HZ375759.TartangaLH.eus>
2024-10-21 12:06:34 +0200

    Menus contextuales IN y MAIN

--- src/vista/ApplicationClientPrincipalController.java
+++ src/vista/ApplicationClientPrincipalController.java
@@ -12,8 +12,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseButton;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import negocio.ApplicationClientFactory;
 import negocio.Client;
@@ -28,9 +32,14 @@ public class ApplicationClientPrincipalController implements Initializable {
     private MenuItem menuCerrarSesion;
     @FXML
     private MenuItem menuSalir;
-
+    @FXML
+    private Label welcomeLabel;
+    @FXML
+    private AnchorPane anchorPane;
     @FXML
     private Button logoutButton;
+
+    private ContextMenu contextMenu;
 
     private Stage stage;
     private Client client;
@@ -39,8 +48,55 @@ public class ApplicationClientPrincipalController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // Acciones para "Cerrar Sesión" y "Salir"
 
+        // Crear el menú contextual personalizado
+        contextMenu = new ContextMenu();
+
+        // Añadir una clase de estilo para el menú contextual
+        contextMenu.getStyleClass().add("context-menu");
+        // Aplicar el mismo estilo que el Tooltip al ContextMenu
+        contextMenu.setStyle("-fx-background-color: rgba(0, 0, 0, 0.8);"
+                + "-fx-text-fill: #FFFFFF;"
+                + "-fx-font-size: 18px;"
+                + "-fx-font-weight: bold;"
+                + "-fx-font-family: 'Protest Strike';"
+                + "-fx-max-width: 250px;"
+                + "-fx-wrap-text: true;"
+                + "-fx-padding: 10px;"
+                + "-fx-border-width: 1;"
+                + "-fx-border-radius: 5;"
+                + "-fx-background-radius: 5;");
+        // Opción "Borrar campos"
+        MenuItem clearFieldsItem = new MenuItem("Cerrar sesión");
+        clearFieldsItem.setStyle("-fx-font-size: 18px;"
+                + "-fx-font-weight: bold;"
+                + "-fx-font-family: 'Protest Strike';"
+                + "-fx-text-fill: #FFFFFF;"
+                + "-fx-background-color: transparent;"
+                + "-fx-max-width: 250px;"
+                + "-fx-wrap-text: true;");
+        clearFieldsItem.setOnAction(event -> cerrarSesion());
+
+        // Opción "Salir"
+        MenuItem exitItem = new MenuItem("Salir");
+        exitItem.setStyle("-fx-font-size: 18px;"
+                + "-fx-font-weight: bold;"
+                + "-fx-font-family: 'Protest Strike';"
+                + "-fx-text-fill: #FFFFFF;"
+                + "-fx-background-color: transparent;"
+                + "-fx-max-width: 250px;"
+                + "-fx-wrap-text: true;");
+        exitItem.setOnAction(event -> salirAplicacion());
+
+        // Añadir las opciones personalizadas al menú contextual
+        contextMenu.getItems().addAll(clearFieldsItem, exitItem);
+
+        // Asignar el menú contextual al GridPane
+        anchorPane.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.SECONDARY) {
+                contextMenu.show(anchorPane, event.getScreenX(), event.getScreenY());
+            }
+        });
     }
 
     public void setClient(Client client) {
@@ -49,6 +105,7 @@ public class ApplicationClientPrincipalController implements Initializable {
 
     public void setUser(User user) {
         this.user = user;
+        welcomeLabel.setText("Bienvenid@ " + user.getName());
     }
 
     public void setStage(Stage stage) {
@@ -84,7 +141,7 @@ public class ApplicationClientPrincipalController implements Initializable {
 
     private void cerrarSesion() {
         // Crear la alerta de confirmación
-        Alert alert = new Alert(Alert.AlertType.WARNING);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Confirmación");
         alert.setHeaderText(null);
         alert.setContentText("¿Estás seguro de que deseas cerrar sesión?");
@@ -99,7 +156,7 @@ public class ApplicationClientPrincipalController implements Initializable {
 
     private void salirAplicacion() {
         // Crear la alerta de confirmación
-        Alert alert = new Alert(Alert.AlertType.WARNING);
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Confirmación");
         alert.setHeaderText(null);
         alert.setContentText("¿Estás seguro de que deseas salir de la aplicación?");
